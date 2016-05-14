7064ddbdad42ca624f154c79fef3edc8b9d9fc0d
peterhoermeyer <peter@code-express.de>
2016-05-14 14:28:01 +0200

    production mode module included

--- app/browsyboot.ts
+++ app/browsyboot.ts
@@ -1,4 +1,6 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {BrowsyComponent} from './components/browsy.component';
+import {enableProdMode} from 'angular2/core';
 
+//enableProdMode();
 bootstrap(BrowsyComponent);
